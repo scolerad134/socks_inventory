074191bcd469ca0028cdfd091e87caa1cb33ac89
scolerad134 <gugac134@gmail.com>
2024-12-18 16:47:06 +0300

    init commit

---
+++ src/main/java/com/socks/SocksInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.socks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksInventoryApplication.class, args);
+	}
+
+}
